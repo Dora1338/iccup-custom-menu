61ee2f85d447a7dabeecf2c975aec3004c3b7dec
Andrey <gbou883@gmail.com>
2023-05-24 00:24:35 +0300

    fixed some layout bugs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,12 +15,13 @@ export const Header = () => {
   }
 
   return (
-    <header className="sticky top-0 h-[72px] transition-all duration-500 bg-gray-50 dark:bg-gray-900">
+    <header className="sticky z-10 top-0 h-[72px] transition-all duration-500 bg-gray-50 dark:bg-gray-900 border-b border-gray-800">
       <div className="mx-auto max-w-8xl xl:px-8">
-        <div className="flex items-center justify-end border-b border-gray-800 px-4 py-5 sm:px-6 lg:px-8 xl:px-0">
+        <div className="flex items-center justify-end px-4 py-5 sm:px-6 lg:px-8 xl:px-0">
           <button
-            className="block py-2 pl-3 pr-4 rounded md:p-0"
+            className="block py-2 pl-3 pr-4 md:p-0"
             onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            type="button"
           >
             {theme === "dark" ? (
               <HiOutlineMoon size={24} />
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -16,7 +16,7 @@ export const Modal: FC<ModalProps> = ({
 }) => {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={() => {}}>
+      <Dialog as="div" className="relative z-50" onClose={() => {}}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
