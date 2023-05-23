5be522c129207e2dd25eb21a12493023fef427f2
Andrey <gbou883@gmail.com>
2023-05-23 23:53:08 +0300

    again

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -5,7 +5,7 @@ export const Layout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
       <Header />
-      <div className="min-h-screen w-full mx-auto justify-between px-4 bg-gray-50 dark:bg-gray-900 sm:px-6 lg:px-8 lg:flex lg:flex-row">
+      <div className="min-h-screen w-full mx-auto justify-between transition-all duration-500 px-4 bg-gray-50 dark:bg-gray-900 sm:px-6 lg:px-8 lg:flex lg:flex-row">
         {children}
       </div>
     </>
