fa2a97914d2eb4a64fa987377c023c5a19461003
Andrey <gbou883@gmail.com>
2023-06-12 18:33:34 +0300

    bug fix: after choosing the custom challenge, the awards of the next tournament were not calculated

--- src/utils/calculateReward.ts
+++ src/utils/calculateReward.ts
@@ -3,63 +3,53 @@ export function calculateReward(
   players: number,
   title: string,
   numOfRounds: number
-) {
-  let reward: number[] = [];
-  if (format === "1x1") {
-    if (title.includes("Custom Challenge")) {
-      if (numOfRounds >= 1 && numOfRounds <= 3) {
-        reward = [80, 60, 40];
-      } else if (numOfRounds === 4) {
-        reward = [100, 80, 60];
-      } else if (numOfRounds >= 5) {
-        reward = [120, 90, 60];
-      }
+): number[] {
+  if (format === "1x1" && title.includes("Custom Challenge")) {
+    if (numOfRounds >= 1 && numOfRounds <= 3) {
+      return [80, 60, 40];
+    } else if (numOfRounds === 4) {
+      return [100, 80, 60];
+    } else if (numOfRounds >= 5) {
+      return [120, 90, 60];
     }
-    return reward;
+  } else {
+    switch (format) {
+      case "1x1":
+        if (players >= 4 && players <= 7) {
+          return [30, 20, 10];
+        } else if (players >= 8 && players <= 13) {
+          return [50, 30, 20];
+        } else if (players >= 14 && players <= 17) {
+          return [70, 50, 30];
+        } else if (players >= 18) {
+          return [80, 60, 40];
+        }
+        break;
+      case "2x2":
+        if (players >= 4 && players <= 7) {
+          return [50, 25, 10];
+        } else if (players >= 8 && players <= 12) {
+          return [70, 50, 30];
+        } else if (players >= 13) {
+          return [80, 60, 40];
+        }
+        break;
+      case "3x3":
+        if (players >= 4 && players <= 6) {
+          return [50, 30, 20];
+        } else if (players >= 7) {
+          return [70, 50, 30];
+        }
+        break;
+      case "5x5":
+        if (players >= 4 && players <= 6) {
+          return [70, 50, 30];
+        } else if (players >= 7) {
+          return [80, 60, 40];
+        }
+        break;
+    }
   }
-  switch (format) {
-    case "1x1":
-      if (players < 4) {
-        reward = [0, 0, 0];
-      } else if (players >= 4 && players <= 7) {
-        reward = [30, 20, 10];
-      } else if (players >= 8 && players <= 13) {
-        reward = [50, 30, 20];
-      } else if (players >= 14 && players <= 17) {
-        reward = [70, 50, 30];
-      } else if (players >= 18) {
-        reward = [80, 60, 40];
-      }
-      break;
-    case "2x2":
-      if (players < 4) {
-        reward = [0, 0, 0];
-      } else if (players >= 4 && players <= 7) {
-        reward = [50, 25, 10];
-      } else if (players >= 8 && players <= 12) {
-        reward = [70, 50, 30];
-      } else if (players >= 13) {
-        reward = [80, 60, 40];
-      }
-      break;
-    case "3x3":
-      if (players < 4) {
-        reward = [0, 0, 0];
-      } else if (players >= 4 && players <= 6) {
-        reward = [50, 30, 20];
-      } else if (players >= 7) {
-        reward = [70, 50, 30];
-      }
-      break;
-    case "5x5":
-      if (players < 4) {
-        reward = [0, 0, 0];
-      } else if (players >= 4 && players <= 6) {
-        reward = [70, 50, 30];
-      } else if (players >= 7) {
-        reward = [80, 60, 40];
-      }
-      break;
-  }
-  return reward;
+
+  return [0, 0, 0];
 }
